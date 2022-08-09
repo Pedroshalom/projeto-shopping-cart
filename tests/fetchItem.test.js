83910e5007465992dd16c82c0ffd7dd934362839
Pedro <pedroshalommrn@hotmail.com>
2022-08-09 16:59:57 -0300

    add req 3

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -4,5 +4,13 @@ const item = require('../mocks/item');
 
 describe('2 - Teste a função fetchItem', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  // fail('Teste vazio');
+  it('Teste se fetchItem é uma função', () => {
+    expect(typeof fetchItem).toBe('function');
+  });
+  
+  it('Execute a função fetchItem com o argumento do item "MLB1615760527" e teste se fetch foi chamada', async () => {
+    await fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalled();
+  });
 });
